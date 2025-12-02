01032010b207cd10c8dc17c1a24ffaf7f7345b3b
burgosjozel3-cpu <burgosjozel3@gmail.com>
2025-12-02 09:17:49 +0800

    Add files via upload

---
+++ jz.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class jz here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class jz
+{
+    public static void main(String[] args){
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+        }
+    }
